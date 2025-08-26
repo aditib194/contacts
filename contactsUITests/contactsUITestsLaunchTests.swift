8d5fdf95f2d7105d9ecb133db407099e67abaae4
aditissh -T git@github.com <aditibansal@umass.edu>
2025-08-26 10:21:04 -0400

    Initial Commit

---
+++ contactsUITests/contactsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  contactsUITestsLaunchTests.swift
+//  contactsUITests
+//
+//  Created by Aditi Bansal on 8/26/25.
+//
+
+import XCTest
+
+final class contactsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
